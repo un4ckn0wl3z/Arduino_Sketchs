faa0db338d793b7f383cac1321f997e99e66b1dc
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2024-08-21 00:18:41 +0700

    feat: add blink and print

--- LED_Bread/LED_Bread.ino
+++ LED_Bread/LED_Bread.ino
@@ -3,12 +3,15 @@
 void setup()
 {
   pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
   delay(100); // Wait for 1000 millisecond(s)
   digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
   delay(100); // Wait for 1000 millisecond(s)
 }
